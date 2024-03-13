dbe38f90bc19bd6073ed96265c2e8eee2eb69c65
Carlos <carloscf.correo@gmail.com>
2024-03-13 18:31:02 +0100

    deleted .vs

---
+++ code/Entity.hpp
@@ -0,0 +1,12 @@
+/*
+    Codigo de dominio público
+    Carlos Cabrera Fernández - 2024
+*/
+
+#pragma once
+
+#include <memory>
+#include <iostream>
+#include <string>
+#include <Box2D/Box2D.h>
+#include <SFML\Graphics.hpp>
---
+++ code/Scene.cpp
@@ -0,0 +1,6 @@
+/*
+    Codigo de dominio público
+    Carlos Cabrera Fernández - 2024
+*/
+
+#pragma once
